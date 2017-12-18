34b48870950ea6fc4b2275eea17cbd8b9603bf18
Durev <vincent.durewski@gmail.com>
2017-12-18 13:34:47 +0100

    Update routes, controller, and add character creation form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,11 @@
 class CharactersController < ApplicationController
   def index
+    @character = Character.new
   end
 
   def arena
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'characters/index'
-
-  get 'characters/arena'
 
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get '/index', to: 'characters#index'
+  get '/arena', to: 'characters#arena'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
