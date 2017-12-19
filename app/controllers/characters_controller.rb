19902959ad96554acd3168c2bb705dd0539b1135
Durev <vincent.durewski@gmail.com>
2017-12-19 18:36:09 +0100

    Allow character creation

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,5 +7,17 @@ class CharactersController < ApplicationController
   end
 
   def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to index_path
+    else
+      render '/index'
+    end
   end
+
+  private
+
+    def character_params
+      params.require(:character).permit(:name, :health_points, :attack_points)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/index', to: 'characters#index'
   get '/arena', to: 'characters#arena'
+  post '/new', to: 'characters#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
