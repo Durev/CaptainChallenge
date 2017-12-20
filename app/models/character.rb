4c4b409a981e7cb30530fc6e253968420bc0c0e3
Durev <vincent.durewski@gmail.com>
2017-12-20 15:49:49 +0100

    Delete useless files

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
-  URL_REGEX = /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/i
+
   validates :name, presence: true, length: { maximum: 70 }
   validates :health_points, presence: true, numericality: { greater_than_or_equal_to: 50,
                                                             less_than_or_equal_to: 100 }
